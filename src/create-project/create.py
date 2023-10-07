db1f64fea9d796ac8e718d70f755da51c7abcb3b
Adam Pócs <royaljerry@gmail.com>
2023-10-08 00:20:11 +0200

    make script work, hooooooooray

--- src/create-project/create.py
+++ src/create-project/create.py
@@ -2,8 +2,9 @@
 
 import sys
 import os
+import datetime
 
-def swtch (current_level, last_level):
+def get_level (current_level, last_level):
 	if current_level == last_level: return "sibling"
 	if current_level > last_level: return "child"
 	if current_level < last_level: return "parent"
@@ -21,6 +22,38 @@ def main():
 	# print('--> script_folder', script_folder)
 	# print('--> project_folder', project_folder)
 	# print('--> data_lines', data_lines)
+	os.chdir(project_folder)
+
+	lLevel = 0
+	sFolder = ""
+	fError = False
+	# Parse lines and create folders
+	for s in data_lines:
+		# ToDo: make this option selectable by an argument
+		cST = s.rstrip("\n")
+		# cST = s.rstrip("\n").replace(" ", "_")
+		cSTemp = cST.replace("%date%", str(datetime.date.today()))
+		cLevel = cSTemp.rfind("\t")+1
+		cS = cSTemp.lstrip("\t")
+		doLev = get_level(cLevel, lLevel)
+		if doLev == "child":
+			os.chdir(sFolder)
+		if doLev == "parent":
+			pDif = lLevel - cLevel
+			for i in range(pDif):
+				os.chdir("..")
+		try:
+			os.mkdir(cS)
+		except Exception:
+			error = True
+		lLevel = cLevel
+		sFolder = cS
+	# print('Creating folders OK')
+	if fError == True:
+		print('Creating folders failed. :(')
+	if fError == False:
+		print('Alfred has created the folders as defined in "' + str(project_folder) + '"')
+
 
 if __name__ == '__main__':
 	main()
