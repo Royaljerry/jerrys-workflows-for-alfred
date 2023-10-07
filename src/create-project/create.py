785848e7d57e7c372d765575dd48e035819dff8d
Adam Pócs <royaljerry@gmail.com>
2023-10-07 23:56:37 +0200

    get arguments

--- src/create-project/create.py
+++ src/create-project/create.py
@@ -9,15 +9,18 @@ def swtch (current_level, last_level):
 	if current_level < last_level: return "parent"
 
 def main():
-	working_folder = os.getcwd()
-	data_file = sys.argv[1]
-	# print(working_folder)
-	# print(data_file)
+	# input
+	script_folder = os.getcwd()
+	project_folder = sys.argv[1]
+	data_file = sys.argv[2]
+	
+	# parse
 	data_object = open(data_file)
 	data_lines = data_object.readlines()
 	data_object.close()
-	print(data_lines)
-
+	# print('--> script_folder', script_folder)
+	# print('--> project_folder', project_folder)
+	# print('--> data_lines', data_lines)
 
 if __name__ == '__main__':
 	main()
